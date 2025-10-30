b76ef9799ea1ac7465d104b595acfedb53b7dad1
JoshuaR132 <jrroberts141@gmail.com>
2025-10-30 00:12:39 +0000

    Created the pages and bug fixing

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js}",
+    "./src/pages/**/*.{astro,html,js}",
+    "./src/components/**/*.{astro,html,js}",
+    "./src/layouts/**/*.{astro,html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
